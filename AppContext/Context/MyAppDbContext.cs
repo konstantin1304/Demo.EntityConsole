5687cb76b07b4c7778eaac7086dc0bb3d05404b4
Konstantin <zaharik_dnepr@yahoo.com>
2018-11-22 22:20:08 +0200

    Changed MyAppDbContext

--- AppContext/Context/MyAppDbContext.cs
+++ AppContext/Context/MyAppDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Demo.Entities;
+using Entities.App;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
@@ -8,8 +9,21 @@ namespace Demo.AppContext
 {
     public class MyAppDbContext:DbContext
     {
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Classroom> Classrooms { get; set; }
         public DbSet<Department> Departments { get; set; }
+        public DbSet<Group> Groups { get; set; }
+        public DbSet<GroupTimetable> GroupTimetables { get; set; }
+        public DbSet<Mark> Marks { get; set; }
+        public DbSet<PairTimetable> PairTimetables { get; set; }
+        public DbSet<Phone> Phones { get; set; }
+        public DbSet<Speciality> Specialities { get; set; }
+        public DbSet<Subject> Subjects { get; set; }
         public DbSet<Teacher> Teachers { get; set; }
+        public DbSet<TeachSubj> TeachSubjs { get; set; }
+        public DbSet<Timetable> Timetables { get; set; }
+
+
 
         public MyAppDbContext(string ConnStrOrDatabaseName)
             :base(ConnStrOrDatabaseName)
